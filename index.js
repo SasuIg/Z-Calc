6acd20e9d1ccd464c623123eafe60304631579f0
unknown <sasuigodan@gmail.com>
2024-05-10 23:50:33 +0100

    added index.html

---
+++ index.js
@@ -0,0 +1,23 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function calculate() {
+    if (display.value.trim() === "") {
+        return;
+    }
+
+    try {
+        display.value = eval(display.value);
+
+    }
+    catch (error) {
+        display.value = "ERROR";
+    }
+}
+
+function clearDisplay() {
+    display.value = "";
+}
